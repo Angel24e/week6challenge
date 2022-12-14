fbb71d7fe0d159ac2e058106ca4c7c767a9f7dc9
Angel Escamilla <angel4esc@gmail.com>
2022-12-14 15:33:39 -0800

    Fixed cards and trying to fix fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
+var APIkey = "d47bf282954b0842d96677fee770a673"
 
-var apiKey = "8219a083f3ff48dd2821a341d20ad1ff"
+var lon;
 
-fetch(url)
-  .then(function (response) {
-     return response.json();
-  })
-  .then(function(data) {
-     console.log(data);
-  });
+var lat;
+
+var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}";
+
+function fetchWeather() {
+   fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      })
+}
+
+fetchWeather();
