a376bd6b24b86455af6c3cb18636f259f16aa480
Angel Escamilla <angel4esc@gmail.com>
2022-12-15 14:39:49 -0800

    Grabbed emojis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
+/*🌞 🌧️ 🌨️ ☁️ emojis for forecast5days*/
 var APIkey = "d47bf282954b0842d96677fee770a673"
 
 var lon;
 
 var lat;
 
-var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}";
+var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d47bf282954b0842d96677fee770a673}";
 
 function fetchWeather() {
    fetch(url)
